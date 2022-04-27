83256b31f1c54d89f999e60c237813b7e4afae6e
Limonya <rare.nuun@gmail.com>
2022-04-27 16:01:29 +0300

    new

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def start():
 @app.route('/index/<varia>')
 def index(varia):
     return render_template('base.html', title=varia)
+@app.route('/profession/<prof>')
+def profi(prof):
+    t = ''
+    if 'инженер' in prof or 'строитель' in prof:
+        t = 'Технарь'
+    else:
+        t = 'Научник'
+    return render_template('base.html', title=prof, profession=t)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
